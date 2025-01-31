34a248f3aa25d09093e6f31982d6ec61b1fa2912
Dmitry <machete75111@gmail.com>
2025-01-31 21:19:10 +0300

    webpack config complete

---
+++ build/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "../src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    mode: "development",
+    devServer: {
+        static: path.resolve(__dirname, "../dist"),
+        hot: true,
+        port: 8080,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../src/index.html"),
+        }),
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+};
